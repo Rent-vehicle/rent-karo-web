9e499897bba2a9b700cbf139c9c625bab7045fc8
Lalit Adhikari <lalit.adhikari@devslane.com>
2025-09-01 17:57:37 +0530

    Redirecting to dashboard after login

--- src/hooks/auth/useGoogleOAuthTokenMutation.ts
+++ src/hooks/auth/useGoogleOAuthTokenMutation.ts
@@ -3,12 +3,17 @@ import { useMutation } from "@tanstack/react-query";
 import { authService } from "@/services/api-services/auth.service";
 import { BaseError } from "@/utils/base-error";
 import { toastService, ToastStyle, ToastType } from "@/services/ToastService";
+import { localStorageService } from "@/services/factories/local-storage.service";
+import { useRouter } from "next/navigation";
 
 export const useGoogleOAuthTokenMutation = () => {
+  const router = useRouter();
   return useMutation<{ token: string }, BaseError, { token: string }>({
     mutationFn: authService.googleOAuthToken,
-    onSuccess: () => {
-      toastService.showToast("Error", ToastType.Error, ToastStyle.Snackbar);
+    onSuccess: (data) => {
+      localStorageService.setAuthToken(data.token);
+      router.push("/home");
+      toastService.showToast("Success", ToastType.Success, ToastStyle.Snackbar);
     },
     onError: (error) => {
       toastService.showToast("Error", ToastType.Error, ToastStyle.Snackbar, error.message);
